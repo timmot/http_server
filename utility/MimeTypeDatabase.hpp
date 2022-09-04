a81d9c99f9198ec5323ab4ea6a7471e3796d7418
timmot <timmot@users.noreply.github.com>
2022-09-04 15:22:53 +1000

    Make libmagic optional

--- utility/MimeTypeDatabase.hpp
+++ utility/MimeTypeDatabase.hpp
@@ -2,7 +2,9 @@
 
 #pragma once
 
+#if __has_include(<magic.h>) && USE_LIBMAGIC
 #include <magic.h>
+#endif
 #include <optional>
 #include <string>
 #include <string_view>
@@ -11,25 +13,35 @@ class MimeTypeDatabase {
 public:
     MimeTypeDatabase()
     {
+#if __has_include(<magic.h>) && USE_LIBMAGIC
         m_magic = magic_open(MAGIC_MIME_TYPE);
         magic_load(m_magic, "/usr/share/misc/magic");
+#endif
     }
 
     ~MimeTypeDatabase()
     {
+#if __has_include(<magic.h>) && USE_LIBMAGIC
         magic_close(m_magic);
+#endif
     }
 
     std::optional<std::string> detect_mime_type(std::string_view filename)
     {
+#if __has_include(<magic.h>) && USE_LIBMAGIC
         auto result = magic_file(m_magic, std::string(filename).c_str());
 
         if (!result)
             return {};
 
         return result;
+#else
+        return {};
+#endif
     }
 
 private:
+#if __has_include(<magic.h>) && USE_LIBMAGIC
     magic_t m_magic;
+#endif
 };
