a32841ce57124eeb4d6f4db0e51e47b5c12270b3
timmot <timmot@users.noreply.github.com>
2022-09-22 14:31:15 +1000

    Cleanup bytes

--- utility/Bytes.hpp
+++ utility/Bytes.hpp
@@ -9,80 +9,111 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <string_view>
 #include <unistd.h>
-
-/*
-how does my Bytes compare to a Span and a ByteBuffer?
-
-a span just stores any T in a C array with a size
-vs an array, a span is considered non-owning
-
-consider buffer as owning as well?
-
-*/
 
 class Bytes {
 public:
-    // TODO: implement move constructor
-    // NOTE: move = default; only works if all values can be moved, this generally means no raw pointers
-    // raw pointers must have their address copied
-    Bytes(Bytes const& bytes)
-        : m_size(bytes.m_size)
+    template <std::size_t N>
+    Bytes(const char (&chars)[N])
     {
-        m_data = static_cast<uint8_t*>(malloc(m_size));
-        memcpy(m_data, bytes.data(), bytes.size());
-    }
-
-    Bytes const& operator=(Bytes const& bytes)
-    {
-        m_size = bytes.size();
-        m_data = static_cast<uint8_t*>(malloc(m_size));
-        memcpy(m_data, bytes.data(), bytes.size());
-        return *this;
+        m_data = new (std::nothrow) uint8_t[N];
+        m_size = N;
+        memcpy(m_data, &chars, N);
     }
 
     Bytes(void const* bytes, size_t size)
         : m_size(size)
     {
-        m_data = static_cast<uint8_t*>(malloc(size));
+        m_data = new (std::nothrow) uint8_t[size];
         memcpy(m_data, bytes, size);
     }
 
-    Bytes(std::string const& string)
+    explicit Bytes(std::string_view string)
         : m_size(string.size())
     {
-        m_data = static_cast<uint8_t*>(malloc(m_size));
+        m_data = new (std::nothrow) uint8_t[m_size];
         memcpy(m_data, string.data(), string.size());
     }
 
     Bytes(size_t size)
     {
-        m_data = (uint8_t*)malloc(size);
+        m_data = new (std::nothrow) uint8_t[size];
         m_size = size;
     }
 
     Bytes()
+        : m_data(nullptr)
+        , m_size(0)
     {
-        m_data = nullptr;
-        m_size = 0;
+    }
+
+    Bytes(Bytes const& bytes)
+        : m_size(bytes.m_size)
+    {
+        m_data = new (std::nothrow) uint8_t[m_size];
+        memcpy(m_data, bytes.data(), bytes.size());
+    }
+
+    Bytes& operator=(Bytes const& bytes)
+    {
+        m_size = bytes.size();
+        m_data = new (std::nothrow) uint8_t[m_size];
+        memcpy(m_data, bytes.data(), bytes.size());
+        return *this;
+    }
+
+    Bytes(Bytes&& other)
+        : m_data(other.m_data)
+        , m_size(std::move(other.m_size))
+    {
+        other.m_data = nullptr;
+        other.m_size = 0;
+    }
+
+    Bytes& operator=(Bytes&& other)
+    {
+        delete[] m_data;
+        m_size = std::move(other.m_size);
+        other.m_size = 0;
+        m_data = other.m_data;
+        other.m_data = nullptr;
+
+        return *this;
     }
 
     ~Bytes()
     {
-        free(m_data);
+        delete[] m_data;
         m_size = 0;
     }
 
-    uint8_t& operator[](size_t i) { return data()[i]; }
+    uint8_t& operator[](size_t i)
+    {
+        return data()[i];
+    }
 
-    uint8_t* data() { return m_data; }
-    uint8_t const* data() const { return m_data; }
-    size_t size() const { return m_size; }
+    uint8_t* data()
+    {
+        return m_data;
+    }
+
+    uint8_t const* data() const
+    {
+        return m_data;
+    }
+
+    size_t size() const
+    {
+        return m_size;
+    }
+
     std::span<uint8_t> span()
     {
         return { data(), size() };
     }
 
+    // TODO: this type feels wrong
     std::optional<size_t> find(uint8_t candidate, size_t length)
     {
         for (size_t i = 0; i < length; ++i)
@@ -104,6 +135,18 @@ public:
         return { m_data + start, m_size - start };
     }
 
+    std::span<uint8_t> slice_span(size_t start, size_t length)
+    {
+        assert(start + length <= m_size);
+        return span().subspan(start, length);
+    }
+
+    std::span<uint8_t> slice_span(size_t start)
+    {
+        assert(start <= m_size);
+        return span().subspan(start, m_size - start);
+    }
+
     void trim(size_t new_size)
     {
         if (new_size < m_size)
@@ -114,6 +157,11 @@ public:
     {
         assert(start + length <= m_size);
         memcpy(m_data + start, data, length);
+    }
+
+    void zero()
+    {
+        memset(m_data, 0, m_size);
     }
 
     uint32_t to_uint32()
@@ -136,7 +184,15 @@ public:
         return static_cast<int16_t>(m_data[0] << 8 | m_data[1]);
     }
 
+    void debug()
+    {
+        printf("Address: %p ; size: %ld\n", (void*)m_data, m_size);
+        for (size_t i = 0; i < size(); ++i) {
+            printf("0x%2X 0d%d %c\n", m_data[i], m_data[i], m_data[i]);
+        }
+    }
+
 private:
     uint8_t* m_data;
-    size_t m_size { 0 };
+    size_t m_size;
 };
