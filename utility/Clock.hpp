845caa5fb9a41253848d6cca19f024caeab408f7
timmot <timmot@users.noreply.github.com>
2023-11-23 23:29:14 +1100

    Utility: Add Random and Clock

---
+++ utility/Clock.hpp
@@ -0,0 +1,57 @@
+// Copyright (c) 202 - Tim Blackstone
+
+#pragma once
+
+#include <chrono>
+
+class Clock {
+public:
+    Clock()
+        : m_time_point(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    void reset()
+    {
+        m_time_point = std::chrono::high_resolution_clock::now();
+    }
+
+    int64_t s_and_reset()
+    {
+        auto value = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+        reset();
+        return value;
+    }
+
+    int64_t ms_and_reset()
+    {
+        auto value = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+        reset();
+        return value;
+    }
+
+    int64_t ns_and_reset()
+    {
+        auto value = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+        reset();
+        return value;
+    }
+
+    int64_t s()
+    {
+        return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+    }
+
+    int64_t ms()
+    {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+    }
+
+    int64_t ns()
+    {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - m_time_point).count();
+    }
+
+private:
+    std::chrono::system_clock::time_point m_time_point;
+};
---
+++ utility/Random.hpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 - Tim Blackstone
+
+#pragma once
+
+#include <concepts>
+#include <random>
+
+class Random {
+public:
+    Random()
+    {
+        std::random_device random_device;
+        m_random_engine = std::minstd_rand0(random_device());
+    }
+
+    template <std::floating_point T>
+    T take_real()
+    {
+        std::uniform_real_distribution<T> m_uniform_distribution;
+        return m_uniform_distribution(m_random_engine);
+    }
+
+    template <std::floating_point T>
+    T take_real(T min, T max)
+    {
+        std::uniform_real_distribution<T> m_uniform_distribution(min, max);
+        return m_uniform_distribution(m_random_engine);
+    }
+
+    template <std::integral T>
+    T take_int()
+    {
+        std::uniform_int_distribution<T> m_uniform_distribution;
+        return m_uniform_distribution(m_random_engine);
+    }
+
+    template <std::integral T>
+    T take_int(T min, T max)
+    {
+        std::uniform_int_distribution<T> m_uniform_distribution(min, max);
+        return m_uniform_distribution(m_random_engine);
+    }
+
+private:
+    std::minstd_rand0 m_random_engine;
+};
